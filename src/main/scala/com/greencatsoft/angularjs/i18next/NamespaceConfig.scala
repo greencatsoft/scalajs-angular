d122916f6ae820d563d4f0c69cd7cfd90eeb0c47
mmx900 <mmx900@gmail.com>
2014-08-01 16:05:59 +0900

    Add i18next module

---
+++ src/main/scala/com/greencatsoft/angularjs/i18next/NamespaceConfig.scala
@@ -0,0 +1,30 @@
+package com.greencatsoft.angularjs.i18next
+
+import scala.scalajs.js
+
+trait NamespaceConfig extends js.Object {
+
+  var namespaces: js.Array[String] = ???
+
+  var defaultNs: String = ???
+}
+
+object NamespaceConfig {
+
+  def apply(namespace: String): NamespaceConfig = {
+    require(namespace != null, "Missing argument 'namespace'.")
+
+    apply(Array(namespace))
+  }
+
+  def apply(namespaces: Array[String], defaultNs: Option[String] = None): NamespaceConfig = {
+    require(namespaces != null, "Missing argument 'namespaces'.")
+
+    val config = new js.Object().asInstanceOf[NamespaceConfig]
+
+    config.namespaces = namespaces
+    config.defaultNs = defaultNs.getOrElse(namespaces(0))
+
+    config
+  }
+}
