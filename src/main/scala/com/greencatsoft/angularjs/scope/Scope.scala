58a82076b9f60e781245e74e22bf8589d5a9d1b2
jokade <jkspam@karchedon.de>
2014-08-23 19:26:28 +0200

    Fixes #6: add missing functions to Scope

--- src/main/scala/com/greencatsoft/angularjs/scope/Scope.scala
+++ src/main/scala/com/greencatsoft/angularjs/scope/Scope.scala
@@ -6,11 +6,30 @@ import com.greencatsoft.angularjs.Injectable
 
 trait Scope extends Injectable {
 
-  def $apply(): js.Any = ???
+  def $id : String = ???
 
-  def $eval(expression: String): js.Any = ???
+  def $apply(exp: js.Any = null): js.Any = ???
+
+  def $broadcast(name: String, args: js.Any*) : js.Object = ???
+
+  def $destroy() :Unit = ???
+
+  def $digest() : Unit = ???
+
+  def $emit(name: String, args: js.Any*) : js.Object = ???
+
+  def $eval(expression: js.Any = null, locals: js.Object = null): js.Any = ???
+
+  def $evalAsync(expression: js.Any = null) : Unit = ???
+
+  def $new(isolate: Boolean) : Scope = ???
 
   def $on(name: String, listener: js.Function): js.Any = ???
+
+  def $watch(watchExpression: js.Any, listener: js.Any = null, objectEquality: Boolean = false) : js.Function = ???
+
+  def $watchCollection(obj: js.Any, listener: js.Function) : js.Function = ???
+
 }
 
 object Scope {
