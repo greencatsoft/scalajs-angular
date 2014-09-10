520ca9cbd6620e9c5f57c90d03809d1bdce1888e
Xavier Cho <mysticfallband@gmail.com>
2014-09-10 19:52:14 +0900

    Allow HTTP PUT/POST/DELETE without payload

--- src/main/scala/com/greencatsoft/angularjs/http/HttpService.scala
+++ src/main/scala/com/greencatsoft/angularjs/http/HttpService.scala
@@ -8,13 +8,19 @@ trait HttpService extends js.Object {
 
   def get(url: String, config: HttpConfig): HttpPromise = ???
 
+  def post(url: String): HttpPromise = ???
+
   def post(url: String, data: js.Any): HttpPromise = ???
 
   def post(url: String, data: js.Any, config: HttpConfig): HttpPromise = ???
 
+  def put(url: String): HttpPromise = ???
+
   def put(url: String, data: js.Any): HttpPromise = ???
 
   def put(url: String, data: js.Any, config: HttpConfig): HttpPromise = ???
+
+  def delete(url: String): HttpPromise = ???
 
   def delete(url: String, data: js.Any): HttpPromise = ???
 
