286fa4942863eedd3fc42160093cfb31e462203d
mmx900 <mmx900@gmail.com>
2015-02-16 17:10:48 +0900

    Add Log and missing providers

--- src/main/scala/com/greencatsoft/angularjs/core/Location.scala
+++ src/main/scala/com/greencatsoft/angularjs/core/Location.scala
@@ -1,8 +1,51 @@
 package com.greencatsoft.angularjs.core
 
+import com.greencatsoft.angularjs.injectable
+
 import scala.scalajs.js
 
-import com.greencatsoft.angularjs.injectable
+/** Use the \$locationProvider to configure how the application deep linking paths are stored.
+  *
+  * @see https://docs.angularjs.org/api/ng/provider/\$locationProvider
+  */
+@injectable("$locationProvider")
+trait LocationProvider extends js.Object {
+
+  /** @param prefix Prefix for hash part (containing path and search)
+    * @return current value if used as getter or itself (chaining) if used as setter
+    */
+  def hashPrefix(prefix: String = null): String = js.native
+
+  /** @param mode If boolean, sets html5Mode.enabled to value. If object, sets enabled, requireBase and rewriteLinks to
+    *             respective values. Supported properties:
+    * @return html5Mode object if used as getter or itself (chaining) if used as setter
+    */
+  def html5Mode(mode: Boolean): Html5ModeInfo = js.native
+
+  /** @param mode If boolean, sets html5Mode.enabled to value. If object, sets enabled, requireBase and rewriteLinks to
+    *             respective values. Supported properties:
+    * @return html5Mode object if used as getter or itself (chaining) if used as setter
+    */
+  def html5Mode(mode: Html5ModeInfo): Html5ModeInfo = js.native
+}
+
+trait Html5ModeInfo extends js.Object {
+
+  /** (default: false) If true, will rely on history.pushState to change urls where supported. Will fall back to
+    * hash-prefixed paths in browsers that do not support pushState.
+    */
+  var enabled: Boolean = js.native
+
+  /** (default: true) When html5Mode is enabled, specifies whether or not a tag is required to be present. If enabled
+    * and requireBase are true, and a base tag is not present, an error will be thrown when \$location is injected. See
+    * the \$location guide for more information
+    */
+  var requireBase: Boolean = js.native
+
+  /** (default: true) When html5Mode is enabled, enables/disables url rewriting for relative links.
+    */
+  var rewriteLinks: Boolean = js.native
+}
 
 @injectable("$location")
 trait Location extends js.Object {
---
+++ src/main/scala/com/greencatsoft/angularjs/core/Log.scala
@@ -0,0 +1,66 @@
+package com.greencatsoft.angularjs.core
+
+import com.greencatsoft.angularjs.injectable
+
+import scala.scalajs.js
+
+/** Use the \$logProvider to configure how the application logs messages
+  *
+  * @see https://docs.angularjs.org/api/ng/provider/\$logProvider
+  */
+@injectable("$logProvider")
+trait LogProvider extends js.Object {
+
+  /** enable or disable debug level messages
+    *
+    * @param flag enable or disable debug level messages
+    * @return current value if used as getter or itself (chaining) if used as setter
+    */
+  def debugEnabled(flag: Boolean): this.type = js.native
+
+  /** enable or disable debug level messages
+    *
+    * @return current value if used as getter or itself (chaining) if used as setter
+    */
+  def debugEnabled(): Boolean = js.native
+}
+
+/** Simple service for logging. Default implementation safely writes the message into the browser's console (if present).
+  * The main purpose of this service is to simplify debugging and troubleshooting.
+  * The default is to log debug messages. You can use ng.\$logProvider#debugEnabled to change this.
+  *
+  * @see https://docs.angularjs.org/api/ng/service/\$log
+  */
+@injectable("$log")
+trait Log extends js.Object {
+
+  /** Write a log message
+    *
+    * @param message
+    */
+  def log(message: js.Any): Unit = js.native
+
+  /** Write an information message
+    *
+    * @param message
+    */
+  def info(message: js.Any): Unit = js.native
+
+  /** Write a warning message
+    *
+    * @param message
+    */
+  def warn(message: js.Any): Unit = js.native
+
+  /** Write an error message
+    *
+    * @param message
+    */
+  def error(message: js.Any): Unit = js.native
+
+  /** Write a debug message
+    *
+    * @param message
+    */
+  def debug(message: js.Any): Unit = js.native
+}
