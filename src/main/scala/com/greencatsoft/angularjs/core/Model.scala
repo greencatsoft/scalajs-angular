f687b86a16a98adb296a2fbce3f9c6b2951de5d5
Xavier Cho <mysticfallband@gmail.com>
2015-04-10 19:48:41 +0900

    Add NgModelController trait

---
+++ src/main/scala/com/greencatsoft/angularjs/core/Model.scala
@@ -0,0 +1,41 @@
+package com.greencatsoft.angularjs.core
+
+import scala.scalajs.js
+import scala.scalajs.js.UndefOr
+
+trait ModelController extends js.Object {
+
+  var $viewValue: UndefOr[String] = js.native
+
+  var $modelValue: js.Any = js.native
+
+  var $parsers: js.Array[js.Function] = js.native
+
+  var $formatters: js.Array[js.Function] = js.native
+
+  var $validators: js.Dictionary[js.Function] = js.native
+
+  var $asyncValidators: js.Dictionary[js.Function2[js.Any, UndefOr[String], Promise]] = js.native
+
+  def $setValidity(validationErrorKey: String, isValid: Boolean): Unit = js.native
+
+  var $viewChangeListeners: js.Array[js.Function] = js.native
+
+  var $error: js.Dictionary[js.Any] = js.native
+
+  var $pending: js.Dictionary[js.Any] = js.native
+
+  var $untouched: Boolean = js.native
+
+  var $touched: Boolean = js.native
+
+  var $pristine: Boolean = js.native
+
+  var $dirty: Boolean = js.native
+
+  var $valid: Boolean = js.native
+
+  var $invalid: Boolean = js.native
+
+  var $name: String = js.native
+}
