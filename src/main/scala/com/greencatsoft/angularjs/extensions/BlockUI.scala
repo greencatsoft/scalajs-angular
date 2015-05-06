1a6eb6cb4c34da3fce2df3adfe22acf21adfc760
Xavier Cho <mysticfallband@gmail.com>
2015-05-06 21:10:04 +0900

    Add support for angular-block-ui

---
+++ src/main/scala/com/greencatsoft/angularjs/extensions/BlockUI.scala
@@ -0,0 +1,51 @@
+package com.greencatsoft.angularjs.extensions
+
+import scala.scalajs.js
+
+import com.greencatsoft.angularjs.core.HttpConfig
+import com.greencatsoft.angularjs.injectable
+
+/**
+ * Wrapper for the angular-block-ui module.
+ *
+ * @see https://github.com/McNull/angular-block-ui
+ */
+@injectable("blockUI")
+trait BlockUI extends js.Object {
+
+  def start(): Unit = js.native
+
+  def start(messsage: String): Unit = js.native
+
+  def stop(): Unit = js.native
+
+  def message(): Unit = js.native
+
+  def done(callback: js.Function0[Unit]): Unit = js.native
+
+  def isBlocking(): Boolean = js.native
+}
+
+@injectable("blockUIConfig")
+trait BlockUIConfig extends js.Object {
+
+  var message: String = js.native
+
+  var delay: Integer = js.native
+
+  var template: String = js.native
+
+  var templateUrl: String = js.native
+
+  var autoBlock: Boolean = js.native
+
+  var resetOnException: Boolean = js.native
+
+  var requestFilter: js.Function1[HttpConfig, Unit] = js.native
+
+  var autoInjectBodyBlock: Boolean = js.native
+
+  var cssClass: String = js.native
+
+  var blockBrowserNavigation: Boolean = js.native
+}
