1f3b13ad60e8c2a7d460c4564c56968db7496f69
Tomas Zeman <tzeman@volny.cz>
2015-10-01 10:47:12 +0200

    $templateCache API

---
+++ src/main/scala/com/greencatsoft/angularjs/core/TemplateCache.scala
@@ -0,0 +1,26 @@
+package com.greencatsoft.angularjs.core
+
+import com.greencatsoft.angularjs.injectable
+
+import scala.scalajs.js
+
+/**
+ * Template cache API.
+ *
+ * @see <a href="https://docs.angularjs.org/api/ng/service/\$templateCache">https://docs.angularjs.org/api/ng/service/\$templateCache</a>
+ */
+@injectable("$templateCache")
+trait TemplateCache extends Cache {
+
+  /**
+   * Inserts a template into the cache.
+   * 
+   * @param id Template id.
+   * @param tpl Template content.
+   * @return The value stored.
+   */
+  def put(id: String, tpl: String): String = js.native
+
+}
+
+// vim: set ts=2 sw=2 et:
