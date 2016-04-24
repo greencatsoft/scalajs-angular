f42eafe783bab6e3233df8d45ae968f11d81c0ff
svenwiegand <source@sven-wiegand.de>
2016-04-24 13:26:18 +0200

    Now I've got it right.

--- src/main/scala/com/greencatsoft/angularjs/Angular.scala
+++ src/main/scala/com/greencatsoft/angularjs/Angular.scala
@@ -16,7 +16,7 @@ object Angular {
   def apply(name: String): Option[Module] =
     angular.module(name).toOption.map(new Module(_))
 
-  def injector: Injector = angular.injector
+  def injector: Injector = angular.injector()
 
   def injector(modules: String*): Injector = angular.injector(modules.toJSArray)
 
