e56c7bef29d56dcc2dde9302f9beff443029cc4d
Xavier Cho <mysticfallband@gmail.com>
2017-01-08 22:09:10 +0900

    Remove a deprecated warning about GlobalScope

--- src/main/scala/com/greencatsoft/angularjs/internal/GlobalDefinitions.scala
+++ src/main/scala/com/greencatsoft/angularjs/internal/GlobalDefinitions.scala
@@ -1,10 +1,11 @@
 package com.greencatsoft.angularjs.internal
 
 import scala.scalajs.js
-import scala.scalajs.js.GlobalScope
+import scala.scalajs.js.annotation.JSGlobalScope
 
 @js.native
-object GlobalDefinitions extends GlobalScope {
+@JSGlobalScope
+object GlobalDefinitions extends js.Object {
 
   private[angularjs] val angular: Angular = js.native
 }
